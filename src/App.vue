7bebb6a27e74d1a366bb931b1d2aea5e2ae4a646
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-06-13 23:05:26 -0400

    internalPropertiesVue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <InternalProperties name="Félix" propiedadquenoestaenprops="esta" />
+</template>
+
+<script>
+import InternalProperties from './components/InternalProperties.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InternalProperties
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InternalProperties.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>InternalProperties<button @click="clickHandler">Click</button></div>
+    <div>:D</div>
+    <div ref="divacio"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InternalProperties",
+  props: {
+    name: String,
+    propiedadquenoestaenprops: String,
+  },
+  data() {
+    return {
+        array: [1,2,3]
+    }
+  },
+  methods: {
+    clickHandler() {
+      //console.log(this);
+      const div = this.$refs.divacio;
+      div.innerHTML = "hola";
+      //console.log("clickHandler");
+    },
+  },
+};
+</script>
